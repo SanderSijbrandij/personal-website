25065263d5ea9b53498a06126b68087c185d389e
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-06 09:39:13 +0100

    restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'static_pages#react_entry'
 
   # Static pages use the link column as primary key.
-  resources :static_pages, param: :link
+  resources :static_pages, param: :link, only: [:index, :show]
 end
