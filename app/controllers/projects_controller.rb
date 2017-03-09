335c056771291606192aff477387837645bb6cbe
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-09 09:37:56 +0100

    fix tags collection on project in serializer and controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
     projects = Project.all
-    render json: projects, include: 'tags'
+    render json: projects, include: :tags
   end
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,4 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :title, :subtitle, :description, :image, :github, :preview
-  
+  has_many :tags
 end
