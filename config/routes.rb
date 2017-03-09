a2ee95b78a0dcb35f7670afde7ee761de1f13f4c
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-09 09:34:24 +0100

    Add controller: projects and generate join table

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # Static pages use the link column as primary key.
   root to: 'static_pages#react_entry'
   resources :static_pages, param: :link, only: [:index, :show]
+  resources :projects, only: [:index]
 
   devise_for :admins
   namespace :admin do
     root to: 'static_pages#react_entry'
     resources :static_pages, param: :link, only: [:create, :update, :destroy]
+    resources :projects, only: [:create, :update, :destroy]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309080632) do
+ActiveRecord::Schema.define(version: 20170309082626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 20170309080632) do
     t.string "preview"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects_tags", id: false, force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.integer "tag_id", null: false
   end
 
   create_table "static_pages", id: false, force: :cascade do |t|
